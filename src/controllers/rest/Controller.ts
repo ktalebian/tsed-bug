6e56cdad08c36c64bc5cc3b98939b3893010f515
Kousha Talebian <kousha.talebian@gmail.com>
2024-05-01 13:00:44 -0700

    happy path

--- src/controllers/rest/Controller.ts
+++ src/controllers/rest/Controller.ts
@@ -1,5 +1,6 @@
 import {Controller, Inject} from "@tsed/di";
-import {CollectionOf, Generics, Get, Property, Returns} from "@tsed/schema";
+import { BodyParams } from "@tsed/platform-params";
+import {CollectionOf, Generics, Get, MaxLength, MinLength, Post, Property, Returns} from "@tsed/schema";
 import { Host } from "src/models/Hosts";
 import { Id } from "src/models/Id";
 import { Page } from "src/models/Page";
@@ -7,14 +8,20 @@ import { PageMeta } from "src/models/PageMeta";
 import { MyService } from "src/services/MyService";
 
 
+class MyRequest {
+  @Property()
+  @MinLength(10).Error('Title must be at least 10 characters long')
+  title: string;
+}
+
 @Controller("/")
 export class HelloWorldController {
   @Inject()
   myService: MyService;
 
-  @Get("/cache")
+  @Post("/body")
   @Returns(200, String)
-  async cache() {
-    return this.myService.doSomething();
+  async cache(@BodyParams() body: MyRequest) {
+    return body;
   }
 }
